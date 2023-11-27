1ae7ecea3905755e5578970bb184411b83927d38
Alexander Zaytsev <me@axv.email>
2023-11-27 18:01:57 +0300

    docker upload script

--- upload_docker.sh
+++ upload_docker.sh
@@ -6,8 +6,8 @@ VERSION="${TAG:1}"
 echo "version: ${VERSION}"
 
 # tag version
-docker tag github.com/z0rr0/spts:latest github.com/z0rr0/spts:${VERSION}
+docker tag z0rr0/spts:latest z0rr0/spts:${VERSION}
 
 # push
-docker push github.com/z0rr0/spts:${VERSION}
-docker push github.com/z0rr0/spts:latest
+docker push z0rr0/spts:${VERSION}
+docker push z0rr0/spts:latest
