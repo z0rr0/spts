6632963f942ee8cc2c16b6f9c16a77c52c2c1b7c
Aleksandr Zaitsev <me@axv.email>
2023-11-29 22:19:59 +0300

    update README

--- upload_docker.sh
+++ upload_docker.sh
@@ -5,9 +5,9 @@ VERSION="${TAG:1}"
 
 echo "version: ${VERSION}"
 
-# tag version
+# add tag as latest version to new image
 docker tag z0rr0/spts:latest z0rr0/spts:${VERSION}
 
-# push
+# send images to docker hub
 docker push z0rr0/spts:${VERSION}
 docker push z0rr0/spts:latest
