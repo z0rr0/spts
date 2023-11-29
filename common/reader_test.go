5798dbeaa2c2123ddc5701079ce4f6de5a961527
Aleksandr Zaitsev <me@axv.email>
2023-11-29 22:25:24 +0300

    correct test

--- common/reader_test.go
+++ common/reader_test.go
@@ -100,9 +100,12 @@ func TestNewReader(t *testing.T) {
 			name:    "timeout",
 			bufSize: 10,
 			ctxFunc: func() context.Context {
-				ctx, cancel := context.WithTimeout(context.Background(), time.Millisecond)
-				defer cancel()
-				time.Sleep(5 * time.Millisecond)
+				ctx, cancel := context.WithTimeout(context.Background(), 5*time.Millisecond)
+				defer func() {
+					time.Sleep(15 * time.Millisecond)
+					cancel()
+				}()
+				time.Sleep(10 * time.Millisecond)
 				return ctx
 			},
 			withError: true,
